71861d1dba471dbbc4d00b064ef7b3d651f984a2
maryjwaters7 <60704985+maryjwaters7@users.noreply.github.com>
2020-05-26 15:39:40 -0400

    updated header and search

---
+++ how-to/src/components/SearchHowTo.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+
+// STYLING ************
+
+
+// CODE *********
+
+const SearchHowTo = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default SearchHowTo;
