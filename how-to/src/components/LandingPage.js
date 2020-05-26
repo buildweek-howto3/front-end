8386ea44986bb03cf665250561619f89991b9e7f
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-26 09:16:34 -0700

    imported three main components to App

---
+++ how-to/src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+export default function LandingPage() {
+    return (
+        <section>
+            <div className="howToCard">
+                <h2>Title</h2>
+                <p>Description goes here.</p>
+            </div>
+        </section>
+    )
+}
