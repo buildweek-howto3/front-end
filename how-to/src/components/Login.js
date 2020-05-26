272fd78197035566e8a7bf6903d5fd8730ccc3ff
maryjwaters7 <60704985+maryjwaters7@users.noreply.github.com>
2020-05-26 19:25:39 -0400

    created skelton structire for login and signup

---
+++ how-to/src/components/Login.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+
+// STYLING ************
+const LoginForm = styled.form`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+`;
+
+// CODE *********
+
+const Login = () => {
+    const [login, setLogin] = useState({username: '', password: ''});
+
+    const handleUsername = event => {
+        setLogin({...login, username: event.target.value});
+    }
+
+    const handlePassword = event => {
+        setLogin({...login, password: event.target.value});
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        
+       
+    }
+
+    return (
+        <div>
+            <LoginForm onSubmit={event => handleSubmit(event)}>
+                <label>
+                    UserName:
+                    <input 
+                    type='text'
+                    name='username'
+                    placeholder="Username"
+                    value={login.username}
+                    onChange={handleUsername}
+                    />
+                </label>
+                <label>
+                    Password
+                    <input 
+                    type='password'
+                    name='password'
+                    placeholder="Password"
+                    value={login.password}
+                    onChange={handlePassword}
+                    />
+                </label>
+                <button>Submit</button>
+            </LoginForm>
+        </div>
+    )
+};
+
+export default Login;
