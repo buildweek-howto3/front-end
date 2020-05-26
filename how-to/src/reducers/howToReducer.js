830f0fb3f2a3d6971433dcfd8e952f62dd7ca3d1
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-26 10:53:52 -0700

    changed shape of dummy data to accomodate multiple steps

--- how-to/src/reducers/howToReducer.js
+++ how-to/src/reducers/howToReducer.js
@@ -5,14 +5,22 @@ const dummydata = {
       category: "example category",
       title: "example 1",
       description: "this explains something",
-      steps: ["step1", "step2"],
+      steps: [
+        { stepName: "step 1", stepDescription: "describes this step" },
+        { stepName: "step 2", stepDescription: "describes this step" },
+        { stepName: "step 3", stepDescription: "describes this step" },
+      ],
     },
     {
       id: "2",
       category: "example category",
       title: "example 2",
       description: "this explains something about the second example",
-      steps: ["step1", "step2"],
+      steps: [
+        { stepName: "step 1", stepDescription: "describes this step" },
+        { stepName: "step 2", stepDescription: "describes this step" },
+        { stepName: "step 3", stepDescription: "describes this step" },
+      ],
     },
   ],
 };
