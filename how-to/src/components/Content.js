c2e2e3d415d755262664d9af6846242e51987788
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-27 09:12:53 -0700

    merging rohiths private route

--- how-to/src/components/Content.js
+++ how-to/src/components/Content.js
@@ -1,41 +1,35 @@
-import React from "react"
-import LandingPage from "./LandingPage"
-import { Route } from "react-router-dom"
-import styled from "styled-components"
+import React from "react";
+import LandingPage from "./LandingPage";
+import { Route } from "react-router-dom";
+import styled from "styled-components";
 
-import CreateHowTo from "./CreateHowTo"
-import Login from "./Login"
-import Signup from "./Signup"
+import Profile from "./Profile";
+import Login from "./Login";
+import Signup from "./Signup";
 
 const ContentSection = styled.div`
-
-    height: 80vh;
-    display: flex;
-    justify-content:center;
-    align-items: center;
-    flex-flow: column;
-    background-color: lightblue;
-    width: 100%;
-
-
-`
-
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+  background-color: lightblue;
+  width: 100%;
+`;
 
 export default function Content() {
-    return (
-        <ContentSection>
-            <Route exact path="/">
-                <LandingPage />
-            </Route>
-            <Route path="/login">
-                <Login />
-            </Route>
-            <Route path="/signup">
-                <Signup />
-            </Route>
-            <Route path="/create-how-to">
-                <CreateHowTo />
-            </Route>
-        </ContentSection>
-    )
+  return (
+    <ContentSection>
+      <Route exact path="/">
+        <LandingPage />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      
+    </ContentSection>
+  );
 }
