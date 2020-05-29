85e61e4799e2a0b43664919414666cea7f832cae
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-28 22:33:29 -0700

    styled the header

--- how-to/src/components/Header.js
+++ how-to/src/components/Header.js
@@ -1,67 +1,79 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-import SearchHowTo from './SearchHowTo'
+import SearchHowTo from "./SearchHowTo";
 
 // STYLING ************
 const HeaderSection = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #414756;
+  h1,
+  a,
+  label,
+  input, button {
+    color: #dcf6f3;
+  }
+  .titleWrapper {
+    width: 100%;
     display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    background-color: dodgerblue;
+    justify-content: space-between;
+    align-items: flex-start;
 
-    .titleWrapper{
-        width:100%;
-        display: flex;
-        justify-content: space-between;
-        align-items: flex-start;
-
-        h1 {
-            padding-left: 2rem;
-        }
-        .navLinks {
-            padding-right: 1rem;
-            display:flex;
-            justify-content: space-evenly;
-            align-items: center;
-            a{
-                margin-right:1rem;
-                border: 2px solid lightgray;
-                border-radius: 10px;
-                padding: 0.3rem;
-                &:hover{
-                    background-color:lightgray;
-                    color:dodgerblue;
-                }
-            }
-        }
+    h1 {
+      padding-left: 2rem;
+      &:hover {
+        color: #33918C;
+        text-shadow: 0 2px 2px #dcf6f3;
+       }
+      
     }
+    .navLinks {
+      padding-right: 1rem;
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      a {
+        margin-right: 1rem;
+        border: 2px solid lightgray;
+        border-radius: 10px;
+        padding: 0.3rem;
+        &:hover {
+          background-color: #33918C;
+        }
+      }
+    }
+  }
 `;
 
 // CODE *********
 
 const Header = () => {
+  const logOut = () => {
+    localStorage.removeItem("token");
+  };
 
-    const logOut = () => {
-        localStorage.removeItem("token")
-    }
+  return (
+    <HeaderSection>
+      <div className="titleWrapper">
+        <Link to="/">
+          <h1>DiY Hacks</h1>
+        </Link>
+        <div className="navLinks">
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/" onClick={logOut}>
+            Logout
+          </Link>
+        </div>
+      </div>
+      <SearchHowTo />
+    </HeaderSection>
+  );
+};
 
-    return (
-        <HeaderSection>
-            <div className='titleWrapper'>
-                <Link to='/'><h1>DiY Hacks</h1></Link>
-                <div className='navLinks'> 
-                    <Link to='/login'>Login</Link>
-                    <Link to='/signup'>Signup</Link>
-                    <Link to='/profile'>Profile</Link>
-                    <Link to='/' onClick={logOut}>Logout</Link>
-                </div>
-             </div>
-            <SearchHowTo />
-        </HeaderSection>
-    )
-}
-
-export default Header
+export default Header;
--- how-to/src/components/SearchHowTo.js
+++ how-to/src/components/SearchHowTo.js
@@ -1,76 +1,71 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-
+import React, { useState } from "react";
+import styled from "styled-components";
 
 // STYLING ************
 const SearchForm = styled.form`
-    margin-top: 3rem;
-    margin-bottom: 3rem;
-    display:flex;
-    justify-content: center;
-    align-items:center; 
+  margin: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    label{
-        font-size: 1.5rem;
-        color: lightgray;
-        margin-right: 0.5rem;
+  label {
+    font-size: 2rem;
+    color: lightgray;
 
-        input{
-            height: 2rem;
-            width: 20rem;
-            margin-left: 0.5rem;
-            border: 2px solid lightgray;
-            background-color:dodgerblue;
-            color:lightgray;
-            font-size: 1.5rem;
-        }
+    input {
+      height: 2rem;
+      width: 20rem;
+      margin: 1rem;
+      border: 2px solid #dcf6f3;
+      background-color: #33918c;
+      color: #dcf6f3;
+      font-size: 2rem;
     }
-    button{
-        background-color: dodgerblue;
-        color:lightgray;
-        padding: 0.2rem;
-        border: 2px solid lightgray;
-        font-size: 1.2rem;
-        &:hover{
-            background-color: lightgray;
-            color:dodgerblue;
-        }
+  }
+  button {
+    background-color: #33918c;
+    color: #dcf6f3;
+    padding: 0.2rem;
+    border: 2px solid #dcf6f3;
+    font-size: 2rem;
+    &:hover {
+      background-color: #dcf6f3;
+      color: #414756;
     }
-
+  }
 `;
 
 // CODE *********
 
 const SearchHowTo = () => {
-    const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
-    const handleSearch = event => {
-        setSearch(event.target.value)
-        console.log(search.terms);
-    }
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+    console.log(search.terms);
+  };
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        // setSearch({terms: ''})
-       
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // setSearch({terms: ''})
+  };
 
-    return (
-        <div>
-            <SearchForm onSubmit={event => handleSubmit(event)}>
-                <label>
-                    Search
-                    <input 
-                    type='text'
-                    placeholder="Search How-To"
-                    value={search.terms}
-                    onChange={handleSearch}
-                    />
-                </label>
-                <button>Search</button>
-            </SearchForm>
-        </div>
-    )
+  return (
+    <div>
+      <SearchForm onSubmit={(event) => handleSubmit(event)}>
+        <label>
+          Search:
+          <input
+            type="text"
+            placeholder="Search How-To"
+            value={search.terms}
+            onChange={handleSearch}
+          />
+        </label>
+        <button>GO!</button>
+      </SearchForm>
+    </div>
+  );
 };
 
 export default SearchHowTo;
